7167e272ef571f9910e9319034c4df339684b1cb
Nikhil Gadakh <nikhil.gadakh23@gmail.com>
2025-10-28 23:40:41 +0530

    Updated main file

--- pms/src/main/java/com/gfss/pms/ProjectManagementSystemApplication.java
+++ pms/src/main/java/com/gfss/pms/ProjectManagementSystemApplication.java
@@ -9,6 +9,7 @@ public class ProjectManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementSystemApplication.class, args);
 		System.out.println("Hello");
+		System.out.println("Hello1");
 	}
 
 
