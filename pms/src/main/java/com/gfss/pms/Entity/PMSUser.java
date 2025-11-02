9f30894664ecd0ea829f17bf86ad4279d7765792
Nikhil Gadakh <nikhil.gadakh23@gmail.com>
2025-11-02 11:23:56 +0530

    Defined PMS User attributes, Repo, And controller basic structure

--- pms/src/main/java/com/gfss/pms/Entity/PMSUser.java
+++ pms/src/main/java/com/gfss/pms/Entity/PMSUser.java
@@ -1,4 +1,35 @@
 package com.gfss.pms.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document(collection = "pms-user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PMSUser {
+    @Id
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String password;
+    private LocalDate dateJoined;
+    private String status;
+
+//    @DBRef
+//    private Role role;
+//
+//    @DBRef
+//    private Department department;
+//
+//    @DBRef
+//    private Project project;
 }
--- pms/src/main/java/com/gfss/pms/Repository/PMSUserRepo.java
+++ pms/src/main/java/com/gfss/pms/Repository/PMSUserRepo.java
@@ -1,6 +1,7 @@
 package com.gfss.pms.Repository;
 
+import com.gfss.pms.Entity.PMSUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PMSUserRepo {
+public interface PMSUserRepo extends MongoRepository<PMSUser,String> {
 }
