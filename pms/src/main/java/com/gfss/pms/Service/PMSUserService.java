305ea61ef5424a89c7b9b7954020f18319270650
Yadnesh25-in <atkaryadnesh143@gmail.com>
2025-12-26 12:05:13 +0530

    PMS User

--- pms/src/main/java/com/gfss/pms/Service/PMSUserService.java
+++ pms/src/main/java/com/gfss/pms/Service/PMSUserService.java
@@ -1,65 +1,20 @@
 package com.gfss.pms.Service;
 
-import com.gfss.pms.Entity.PMSUser;
-import com.gfss.pms.Repository.PMSUserRepo;
 import com.gfss.pms.dto.PMSUserRequestDTO;
 import com.gfss.pms.dto.PMSUserResponseDTO;
-import com.gfss.pms.mapper.PMSUserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-@Service
-public class PMSUserService {
+public interface PMSUserService {
 
-    @Autowired
-    private PMSUserRepo userRepo;
+    PMSUserResponseDTO createUser(PMSUserRequestDTO requestDTO);
 
-    //  Create user
-    public PMSUserResponseDTO createUser(PMSUserRequestDTO requestDTO) {
-        PMSUser user = PMSUserMapper.toEntity(requestDTO);
-        PMSUser savedUser = userRepo.save(user);
-        return PMSUserMapper.toResponseDTO(savedUser);
-    }
+    List<PMSUserResponseDTO> getAllUsers();
 
-    // Get all users
-    public List<PMSUserResponseDTO> getAllUsers() {
-        return userRepo.findAll()
-                .stream()
-                .map(PMSUserMapper::toResponseDTO)
-                .collect(Collectors.toList());
-    }
+    Optional<PMSUserResponseDTO> getUserById(String id);
 
-    //  Get user by ID
-    public Optional<PMSUserResponseDTO> getUserById(String id) {
-        return userRepo.findById(id)
-                .map(PMSUserMapper::toResponseDTO);
-    }
+    Optional<PMSUserResponseDTO> updateUser(String id, PMSUserRequestDTO requestDTO);
 
-    //  Update user
-    public Optional<PMSUserResponseDTO> updateUser(String id, PMSUserRequestDTO requestDTO) {
-        Optional<PMSUser> existingUser = userRepo.findById(id);
-        if (existingUser.isPresent()) {
-            PMSUser user = existingUser.get();
-            if (requestDTO.getFirstName() != null) user.setFirstName(requestDTO.getFirstName());
-            if (requestDTO.getLastName() != null) user.setLastName(requestDTO.getLastName());
-            if (requestDTO.getEmail() != null) user.setEmail(requestDTO.getEmail());
-            if (requestDTO.getPhone() != null) user.setPhone(requestDTO.getPhone());
-            if (requestDTO.getPassword() != null) user.setPassword(requestDTO.getPassword());
-            if (requestDTO.getDateJoined() != null) user.setDateJoined(requestDTO.getDateJoined());
-            if (requestDTO.getStatus() != null) user.setStatus(requestDTO.getStatus());
-
-            PMSUser updated = userRepo.save(user);
-            return Optional.of(PMSUserMapper.toResponseDTO(updated));
-        }
-        return Optional.empty();
-    }
-
-    //  Delete user
-    public void deleteUser(String id) {
-        userRepo.deleteById(id);
-    }
+    void deleteUser(String id);
 }
