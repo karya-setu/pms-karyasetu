50232aa957def960c81cd71f585c1e8dd1225f8d
Yadnesh25-in <atkaryadnesh143@gmail.com>
2025-12-31 11:41:33 +0530

    Project module done
    CRUD Operation Done

---
+++ pms/src/main/java/com/gfss/pms/Controller/ProjectController.java
@@ -0,0 +1,62 @@
+package com.gfss.pms.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gfss.pms.Entity.Project;
+import com.gfss.pms.Service.ProjectService;
+
+
+
+@RestController
+@RequestMapping("/api/projects")
+@CrossOrigin(origins = "*")
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+//api
+    // CREATE
+    @PostMapping
+    public ResponseEntity<Project> createProject(@RequestBody Project project) {
+        return ResponseEntity.ok(projectService.createProject(project));
+    }
+
+    // READ ALL
+    @GetMapping
+    public ResponseEntity<List<Project>> getAllProjects() {
+        return ResponseEntity.ok(projectService.getAllProjects());
+    }
+
+    // READ ONE
+    @GetMapping("/{id}")
+    public ResponseEntity<Project> getProjectById(@PathVariable String id) {
+        return ResponseEntity.ok(projectService.getProjectById(id));
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<Project> updateProject(@PathVariable String id, @RequestBody Project project) {
+        return ResponseEntity.ok(projectService.updateProject(id, project));
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProject(@PathVariable String id) {
+        projectService.deleteProject(id);
+        return ResponseEntity.ok("Project deleted successfully!");
+    }
+}
